d2e7510825c9cafbecf51c773731c67d2e7fd961
aliuwahab <aliuwahab@yahoo.com>
2015-08-16 21:57:33 +0000

    After setting up database connection and users db structure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->string('phone_number');
+            $table->string('country');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
