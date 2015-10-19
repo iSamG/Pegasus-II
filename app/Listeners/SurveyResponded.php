b7cadc2ca7da91e504ece3030f163d4022a5f134
aliuwahab <aliuwahab@yahoo.com>
2015-10-19 09:08:31 +0000

    After working on creating pusher channel for when a survey has been responded to

---
+++ app/Listeners/SurveyResponded.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\NewSurveyResponse;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SurveyResponded
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  NewSurveyResponse  $event
+     * @return void
+     */
+    public function handle(NewSurveyResponse $event)
+    {
+        //
+    }
+}
